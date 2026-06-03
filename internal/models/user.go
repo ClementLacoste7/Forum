bd8e74587254753a9f3a0c410a0150f99c3264c2
Clement <clement.lacoste64@gmail.com>
2026-06-03 22:27:34 +0200

    add reset token fields to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,14 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"uniqueIndex;not null"`
-	Email    string `gorm:"uniqueIndex;not null"`
-	Password string `gorm:"not null"`
-	Avatar   string
-	Posts    []Post    `gorm:"foreignKey:UserID"`
-	Comments []Comment `gorm:"foreignKey:UserID"`
-	Likes    []Like    `gorm:"foreignKey:UserID"`
+	Username           string     `gorm:"uniqueIndex;not null"`
+	Email              string     `gorm:"uniqueIndex;not null"`
+	Password           string     `gorm:"not null"`
+	Avatar             string
+	ResetToken         string
+	ResetTokenExpiry   time.Time
+	Posts              []Post    `gorm:"foreignKey:UserID"`
+	Comments           []Comment `gorm:"foreignKey:UserID"`
+	Likes              []Like    `gorm:"foreignKey:UserID"`
 }
